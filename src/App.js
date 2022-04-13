019a9b8442a0532bed86014e9bbc70b8a650d01a
Erinn Schneider <erinnaschneider@gmail.com>
2022-04-13 12:33:01 -0400

    adding the app function to render components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import About from './components/About';
+import Contact from './components/Contact';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { useState } from 'react';
 
 function App() {
+  const [navOptions] = useState(['About', 'Portfolio', 'Contact', 'Resume']);
+  
+  // set default navigation selection to the About Me section
+  const [currentNav, setCurrentNav] = useState(navOptions[0]);
+
+  function renderCurrentNav() {
+    switch (currentNav) {
+      case 'About':
+        return (
+          <About></About>
+        );
+      case 'Portfolio':
+        return (
+          <Portfolio></Portfolio>
+        );
+      case 'Contact':
+        return (
+          <Contact></Contact>
+        );
+      case 'Resume':
+        return (
+          <Resume></Resume>
+        );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='page-container'>
+      <div className='content-wrap'>
+        <Header
+          navOptions={navOptions}
+          currentNav={currentNav}
+          setCurrentNav={setCurrentNav}
+        ></Header>
+        <main>
+          {/* conditionally render components here */}
+          {renderCurrentNav()}
+        </main>
+      </div>
+      {currentNav !== 'Contact' && 
+        <Footer></Footer>
+      }
     </div>
   );
 }
